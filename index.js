1b4d77cea5e647e8320f91cccd4d3faf55bb0b7f
Frode Bjerke <frode.bjerke@gmail.com>
2016-01-14 10:07:01 +0100

    Use comic title and set timing with env

--- index.js
+++ index.js
@@ -3,14 +3,13 @@
 var channelUrl = process.env.CHANNEL;
 var channelName = process.env.CHANNEL_NAME || '#comics';
 var channelUsername = 'dilbert';
-var messagePretext = 'Your daily dose of office humor ';
-var messageTitle = 'Daily Dilbert';
+var messagePretext = 'Dagens Dilbert';
 var slack = require('slack-notify')(channelUrl);
 var request = require('request');
 var cheerio = require('cheerio');
 var endpoint = 'http://dilbert.com';
-var cronTime = '00 00 11 * * 1-7';
-var timeZone = 'America/New_York';
+var cronTime = process.env.CRON_TIME || '00 37 13 * * 1-7';
+var timeZone = process.env.TIME_ZONE || 'Europe/Oslo';
 var port = process.env.PORT || 80;
 ////end configurations
 var http = require('http');
@@ -19,37 +18,7 @@ var index = fs.readFileSync('index.html');
 var CronJob = require('cron').CronJob;
 var job = new CronJob({
 	cronTime: cronTime,
-	onTick: function() {
-		request(endpoint, function(error, response, body) {
-			if (error && response.statusCode != 200) {
-				return console.log('ERROR ', response.statusCode)
-			}
-			try {
-				$ = cheerio.load(body);
-			} catch (e) {
-				return console.log('ERROR ', e)
-			}
-			var src = $('img.img-comic').first().attr('src');
-			console.log(src);
-			var payload = {
-				channel: channelName,
-				username: channelUsername,
-				"attachments": [{
-					"fallback": "Required plain-text summary of the attachment.",
-					"color": "#36a64f",
-					"pretext": messagePretext,
-					"title": messageTitle,
-					"image_url": src
-				}]
-			}
-
-			slack.send(payload, function() {
-				console.log("Slack message sent");
-			});
-		});
-
-
-	},
+	onTick: fetchDilbert,
 	start: true,
 	timeZone: timeZone
 });
@@ -58,3 +27,36 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end(index);
 }).listen(port);
+
+function fetchDilbert() {
+	request(endpoint, function(error, response, body) {
+		if (error && response.statusCode != 200) {
+			return console.log('ERROR ', response.statusCode)
+		}
+		try {
+			$ = cheerio.load(body);
+		} catch (e) {
+			return console.log('ERROR ', e)
+		}
+		var src = $('img.img-comic').first().attr('src');
+		var title = $('.comic-title-name').first().text();
+
+		var payload = {
+			channel: channelName,
+			username: channelUsername,
+			"attachments": [{
+				"fallback": "Required plain-text summary of the attachment.",
+				"color": "#36a64f",
+				"pretext": messagePretext,
+				"title": title,
+				"image_url": src
+			}]
+		}
+
+		slack.send(payload, function() {
+			console.log("Slack message sent");
+		});
+	});
+
+
+}
